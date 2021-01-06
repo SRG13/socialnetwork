857b46ae468d8b5eed427d8ff96ad5dbd0413c73
SRG13 <freaking.vrench@gmail.com>
2021-01-06 12:04:53 +0300

    Initial commit

---
+++ src/main/java/com/github/srg13/socialnetwork/SocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.github.srg13.socialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/srg13/socialnetwork/SocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.srg13.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
