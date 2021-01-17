2eeb36295fea29472bb85ad7d19373bd1e744b00
SRG13 <freaking.vrench@gmail.com>
2021-01-17 15:32:10 +0300

    add db migration

---
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS users_roles;
+DROP TABLE IF EXISTS users;
+DROP SEQUENCE IF EXISTS global_seq;
+
+CREATE SEQUENCE global_seq START WITH 100000 INCREMENT 1;
+
+CREATE TABLE users
+(
+    id              int4            PRIMARY KEY DEFAULT nextval('global_seq'),
+    username        VARCHAR(15)             NOT NULL,
+    email           VARCHAR(255)    UNIQUE  NOT NULL,
+    password        VARCHAR(255)            NOT NULL,
+    first_name      VARCHAR(255)            NOT NULL,
+    last_name       VARCHAR(255)            NOT NULL,
+    enabled         boolean DEFAULT FALSE   NOT NULL,
+    profile_image   varchar(255),
+    activation_code VARCHAR(255)
+);
+CREATE UNIQUE INDEX users_unique_username_idx ON users (username);
+
+CREATE TABLE users_roles
+(
+    user_id int4    NOT NULL,
+    role    VARCHAR NOT NULL,
+    CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
